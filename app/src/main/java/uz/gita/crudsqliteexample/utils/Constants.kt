61a34ca8fc7d9d2485ffcd0575770ad2966f2db6
DAVID <nabijonovo23@gmail.com>
2023-03-27 16:05:42 +0300

    project updated

--- app/src/main/java/uz/gita/crudsqliteexample/utils/Constants.kt
+++ app/src/main/java/uz/gita/crudsqliteexample/utils/Constants.kt
@@ -2,8 +2,8 @@ package uz.gita.crudsqliteexample.utils
 
 object Constants {
 
-    val DB_NAME = "Userdata"
-    val TABLE_NAME = "Users"
-    val NAME = "name"
-    val NUMBER = "number"
+    const val DB_NAME = "Userdata"
+    const val TABLE_NAME = "Users"
+    const val NAME = "name"
+    const val NUMBER = "number"
 }
